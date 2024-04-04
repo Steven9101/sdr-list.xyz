3da72c2fec6520fad57ca5e10d443a396f425687
Z1EJ200 <156682521+Z1EJ200@users.noreply.github.com>
2024-04-05 01:05:56 +0200

    fixes
    design changes, long text truncate, sorting by user count

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,40 +21,47 @@ function getLink(receiver) {
 
 
 function getRangeText(receiver) {
-  let start = (receiver.center_frequency - receiver.bandwidth / 2) / 100;
-  let end = (receiver.center_frequency + receiver.bandwidth / 2) / 1000;
+  let start = (receiver.center_frequency - receiver.bandwidth / 2) / 1000000;
+  let end = (receiver.center_frequency + receiver.bandwidth / 2) / 1000000;
 
-  return `${start} - ${end}`;
+  return `${start.toFixed(2)} - ${end.toFixed(2)} mhz`;
 }
 
 function checkReceiverFilter(filters, receiver) {
-
+  // Convert receiver frequency range from Hz to kHz and calculate start and end frequencies.
   let receiverStart = (receiver.center_frequency - receiver.bandwidth / 2) / 1000;
   let receiverEnd = (receiver.center_frequency + receiver.bandwidth / 2) / 1000;
 
+  // Check if no filters are selected; if none, all receivers are considered a match.
   const noFilterSelected = Object.values(filters).every(value => value === false);
   if (noFilterSelected) {
     return true;
   }
 
-  if (filters['160m'] && !(receiverStart <= 2000 && receiverEnd >= 1800)) {
+  // Log the receiver's name and frequency range for debugging.
+  console.log(receiver.name, receiverStart, receiverEnd);
+
+  // Check each band filter to ensure the receiver fully covers the band.
+  if (filters['160m'] && (receiverStart > 1800 || receiverEnd < 2000)) {
     return false;
   }
-  if (filters['80m'] && !(receiverStart <= 4000 && receiverEnd >= 3500)) {
+  if (filters['80m'] && (receiverStart > 3500 || receiverEnd < 4000)) {
     return false;
   }
-  if (filters['40m'] && !(receiverStart <= 7300 && receiverEnd >= 7000)) {
+  if (filters['40m'] && (receiverStart > 7000 || receiverEnd < 7300)) {
     return false;
   }
-  if (filters['20m'] && !(receiverStart <= 14350 && receiverEnd >= 14000)) {
+  if (filters['20m'] && (receiverStart > 14000 || receiverEnd < 14350)) {
     return false;
   }
-  if (filters['2m'] && !(receiverStart <= 174000 && receiverEnd >= 165000)) {
+  if (filters['2m'] && (receiverStart > 144000 || receiverEnd < 148000)) { // Adjusted values to typical 2m band
     return false;
   }
 
+  // If none of the conditions triggered a false return, the receiver is considered a match.
   return true;
 }
+
 
 
 function getTotalUserCount(receivers) {
@@ -71,9 +78,9 @@ function Stats(props) {
 
     <div className='text-white mt-5 rounded bg-slate-800 w-72'>
 
-      <p className='p-2 bg-sky-500 text-center h-6 flex items-center justify-center rounded-t'><i className="fa-solid fa-circle-info w-6 -h"></i> Stats</p>
+      <p className='font-bold p-2 bg-gradient-to-r from-sky-500 to-indigo-400 text-center h-6 flex items-center justify-center rounded-t'><i className="fa-solid fa-circle-info w-6 -h"></i> Stats</p>
 
-      <div className='flex flex-col p-2 border-l border-b border-r border-sky-500'>
+      <div className='flex flex-col p-2 border-l border-b border-r rounded-b border-sky-500'>
         <table className='text-white w-full'>
 
           <tbody>
@@ -124,13 +131,13 @@ function Filters(props) {
 
     <div className='text-white mt-5 rounded bg-slate-800 w-72'>
 
-      <p className='p-2 bg-indigo-400 text-center h-6 flex items-center justify-center rounded-t'><i className="fa-solid fa-filter w-6"></i> Filters</p>
+      <p className='font-bold p-2 bg-gradient-to-r from-sky-500 to-indigo-400 text-center h-6 flex items-center justify-center rounded-t'><i className="fa-solid fa-filter w-6"></i> Filters</p>
 
-      <div className='flex flex-col items-center justify-between p-2 border-l border-b border-r border-indigo-400'>
+      <div className='flex flex-col items-center justify-between rounded-b p-2 border-l border-b border-r border-sky-400'>
         {Object.keys(props.filters).map((band) => (
           <button
             key={band}
-            className={`block w-full my-1 rounded border border-slate-600 ${props.filters[band] ? 'bg-green-400  text-black font-bold' : 'bg-slate-800'}`}
+            className={`block w-full my-1 rounded border border-slate-600 ${props.filters[band] ? 'bg-green-400  text-black font-bold' : 'bg-gradient-to-r from-slate-700 to-slate-800'}`}
             onClick={() => toggleFilter(band)}
           >
             {band}
@@ -143,24 +150,28 @@ function Filters(props) {
   )
 }
 
+function getSortedAndFilteredReceiverList(props) {
+
+  const receiverlist = props.receivers
+    .filter(receiver => checkReceiverFilter(props.filters, receiver))
+    .sort((a, b) => b.users - a.users); // Sorting in descending order by user count
+
+  return receiverlist;
+}
+
 
 function SDRList(props) {
 
-  const filteredReceivers = props.receivers.filter(receiver => checkReceiverFilter(props.filters, receiver));
-
-
-  
   return (
 
+    <div className='text-white flex flex-col justify-center items-center w-72 my-2'>
 
-    <div className='text-white flex flex-col justify-center items-center w-72 mt-5'>
+      {getSortedAndFilteredReceiverList(props).map((receiver) =>
 
-      {filteredReceivers.map((receiver) =>
-
-        <a key={receiver.id} className='relative border border-slate-600 p-2 rounded bg-slate-800 m-1 w-full' target="_blank" href={getLink(receiver)}>
+        <a key={receiver.id} className='relative shadow-inner shadow-slate-700 border border-slate-600 p-2 rounded bg-slate-800 m-1 w-full mt-5' target="_blank" href={getLink(receiver)}>
 
           <div className='flex items-center justify-center'>
-            <p className='font-bold inline'>{receiver.name}</p>
+            <p className='font-bold truncate'>{receiver.name}</p>
           </div>
 
           <div id="stats" className='flex justify-center items-center flex-col mt-2'>
@@ -173,69 +184,60 @@ function SDRList(props) {
                   <td className='flex'>
 
                     <div className='flex justify-center items-center w-6'>
-                     <i className="fa-solid fa-location-dot"></i>
+                      <i className="fa-solid fa-location-dot"></i>
                     </div>
-             
+
                     <span className='pl-2'>Location</span>
                   </td>
                   <td>
-                    <img className='w-6 block bg-slate-100' src={getFlagImage(receiver)}/>
+                    <img className='w-6 block bg-slate-100' src={getFlagImage(receiver)} />
                   </td>
                 </tr>
-
 
                 <tr className='border-b border-slate-700'>
                   <td className='flex'>
 
                     <div className='flex justify-center items-center w-6'>
-                     <i className="fa-solid fa-left-right"></i>
+                      <i className="fa-solid fa-left-right"></i>
                     </div>
-             
+
                     <span className='pl-2'>Range</span>
                   </td>
                   <td>
-                  {getRangeText(receiver)}
+                    {getRangeText(receiver)}
                   </td>
                 </tr>
-
-
-
-
 
                 <tr className='border-b border-slate-700'>
                   <td className='flex'>
 
                     <div className='flex justify-center items-center w-6'>
-                     <i className="fa-solid fa-tower-cell"></i>
+                      <i className="fa-solid fa-tower-cell"></i>
                     </div>
-                    
+
                     <span className='pl-2'>Antenna</span>
                   </td>
                   <td>
-                  {receiver.antenna}
+                    {receiver.antenna}
                   </td>
                 </tr>
-
-
-
-                
 
                 <tr>
                   <td className='flex'>
 
                     <div className='flex justify-center items-center w-6'>
-                     <i className="fa-regular fa-user"></i>
+                      <i className="fa-regular fa-user"></i>
                     </div>
-                    
+
                     <span className='pl-2'>Users</span>
                   </td>
                   <td>
-                  {receiver.users}
+                    {receiver.users}
                   </td>
                 </tr>
 
-           
-   
+
+
               </tbody>
 
             </table>
@@ -307,70 +309,71 @@ function gridLocatorToLatLng(gridLocator) {
 }
 
 
-
 function SDRMap(props) {
-  const filteredReceivers = props.receivers.filter(receiver => checkReceiverFilter(props.filters, receiver));
-
-
 
   return (
-    <div className='bg-slate-500 mt-5 w-72 h-72 sm:w-full sm:h-96'>
-      <MapContainer className='w-full h-full' center={[51.00, 9.00]} zoom={2} scrollWheelZoom={true}>
+    <div className='rounded-2xl bg-slate-500 mt-5 w-full h-96' >
+      <MapContainer className='rounded-2xl w-full h-full' center={[51.00, 9.00]} zoom={2} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {filteredReceivers.map(receiver => (
 
+        {getSortedAndFilteredReceiverList(props).map(receiver => (
 
           <Marker key={receiver.id} position={gridLocatorToLatLng(receiver.grid_locator)}>
+
             <Popup className='w-64' minWidth={64}>
-
-
               <h1 className='font-bold mb-2 text-center'> {receiver.name}</h1>
 
-
-
               <div>
-                <span className='inline-block w-32'>Antenna</span>
+                <span className='inline-block w-28'>Antenna</span>
                 <span className='inline'>{receiver.antenna}</span>
               </div>
 
               <div>
-                <span className='inline-block w-32'>Range</span>
+                <span className='inline-block w-28'>Range</span>
                 <span className='inline'>{getRangeText(receiver)}</span>
               </div>
 
               <div>
-                <span className='inline-block w-32'>Users</span>
+                <span className='inline-block w-28'>Users</span>
                 <span className='inline'>{receiver.users}</span>
               </div>
-
-
 
               <a className='mt-2 block w-full bg-slate-300 uppercase font-thin rounded p-1 text-center' target="_blank" href={getLink(receiver)}>Open</a>
 
             </Popup>
           </Marker>
         ))}
+
       </MapContainer>
     </div>
   );
 }
 
 function ModeSwitchButton(props) {
-
-  let text = "";
-
-  if (props.mapView) {
-    text = "Map Mode"
-  } else {
-    text = "List Mode"
-  }
   return (
-    <button className='bg-slate-800 w-72 px-2 py-1 border border-slate-600 rounded text-white mt-5 mx-0' onClick={() => props.setMapView(!props.mapView)}>{text}</button>
+    <div className='flex text-white mt-5'>
+
+      <div className='flex justify-center items-center'>
+        <i className="fa-solid fa-list"></i>
+        <p className='ml-2'>List</p>
+      </div>
+
+      <div className="relative flex items-center w-12 rounded-full bg-slate-500 border border-slate-400 mx-2 cursor-pointer " onClick={() => props.setMapView(!props.mapView)}>
+        <div className={"absolute w-4 h-4 rounded-full bg-slate-200 border border-slate-700 m-1 " + (props.mapView ? 'right-0' : 'left-0')}></div>
+      </div>
+
+      <div className='flex justify-center items-center'>
+        <i className="fa-solid fa-map"></i>
+        <p className='ml-2'>Map</p>
+      </div>
+
+    </div>
   )
 }
+
 
 function CurrentlyNoReceivers() {
   return (
@@ -382,12 +385,11 @@ function CurrentlyNoReceivers() {
   )
 }
 
-function getFlagImage(receiver){
+function getFlagImage(receiver) {
   return `https://flagcdn.com/${receiver.country}.svg`;
 }
 
-
-async function getCountry(receiver){
+async function getCountry(receiver) {
   try {
     let response = await fetch(`https://api.country.is/${receiver.ip}`);
     if (!response.ok) {
@@ -421,54 +423,46 @@ function App() {
         receiver.country = await getCountry(receiver) || receiver.country;
         return receiver;
       });
-  
+
       Promise.all(updates).then(updatedReceivers => {
         setReceivers(updatedReceivers);
       });
     };
-  
+
     fetchData();
-    const intervalId = setInterval(fetchData, 10000); // Update interval set to 10 seconds
-  
+    const intervalId = setInterval(fetchData, 1000 * 10);
+
     return () => clearInterval(intervalId);
   }, []);
 
   return (
     <>
-
       <div className='bg-slate-800 w-full border-b border-slate-600 p-2'>
         <h1 className='text-white text-center font-bold'>SDR List</h1>
       </div>
 
-      <div className='flex  flex-col items-center justify-center select-none m-5'>
+      <div className='flex flex-col items-center justify-center select-none m-5'>
 
         <a href="https://github.com/Steven9101/PhantomSDR-Plus" className='font-bold text-center mt-3 text-sky-500 underline'><i className="fa-solid fa-link"></i> Guide: Host Your Own SDR</a>
 
-        <Stats receivers={receivers}></Stats>
-        <Filters filters={filters} setFilters={setFilters}></Filters>
+        {receivers.length === 0 ? (
+          <CurrentlyNoReceivers></CurrentlyNoReceivers>
+        ) : (
+          <>
 
-        <ModeSwitchButton mapView={mapView} setMapView={setMapView}></ModeSwitchButton>
+            <Stats receivers={receivers}></Stats>
+            <Filters filters={filters} setFilters={setFilters}></Filters>
+            <ModeSwitchButton mapView={mapView} setMapView={setMapView}></ModeSwitchButton>
 
-        {
-          (() => {
+            {mapView ? (
+              <SDRMap receivers={receivers} filters={filters}></SDRMap>
+            ) : (
+              <SDRList receivers={receivers} filters={filters}></SDRList>
+            )}
 
-            if (receivers.length == 0) {
-              return <CurrentlyNoReceivers></CurrentlyNoReceivers>
-            }
-
-
-            if (mapView) {
-              return <SDRMap receivers={receivers} filters={filters}></SDRMap>
-            }
-
-            return <SDRList receivers={receivers} filters={filters}></SDRList>
-
-          })()
-
-        }
-
+          </>
+        )}
       </div>
-
     </>
   );
 }
