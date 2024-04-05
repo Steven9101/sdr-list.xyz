b4a42e4db11f874d6eb41239e2f11d3944a315cc
Z1EJ200 <156682521+Z1EJ200@users.noreply.github.com>
2024-04-05 16:44:16 +0200

    fixed
    - remove bands filter, added frequency filter input 
    - made map bigger
    - added light mode and dark/light toggle
    - removed stats

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,25 @@ function getLink(receiver) {
 }
 
 
+function getFlagImage(receiver) {
+  return `https://flagcdn.com/${receiver.country}.svg`;
+}
+
+async function getCountry(receiver) {
+  try {
+    let response = await fetch(`https://api.country.is/${receiver.ip}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    let json = await response.json();
+    return json.country.toLowerCase();
+  } catch (error) {
+    console.error("Could not fetch country: ", error);
+  }
+}
+
+
+
 
 function getRangeText(receiver) {
   let start = (receiver.center_frequency - receiver.bandwidth / 2) / 1000000;
@@ -27,160 +46,123 @@ function getRangeText(receiver) {
   return `${start.toFixed(2)} - ${end.toFixed(2)} mhz`;
 }
 
-function checkReceiverFilter(filters, receiver) {
-  // Convert receiver frequency range from Hz to kHz and calculate start and end frequencies.
-  let receiverStart = (receiver.center_frequency - receiver.bandwidth / 2) / 1000;
-  let receiverEnd = (receiver.center_frequency + receiver.bandwidth / 2) / 1000;
+function checkReceiverFilter(filterFrequency, receiver) {
 
-  // Check if no filters are selected; if none, all receivers are considered a match.
-  const noFilterSelected = Object.values(filters).every(value => value === false);
-  if (noFilterSelected) {
+  if (filterFrequency === -1) {
     return true;
   }
 
-  // Log the receiver's name and frequency range for debugging.
-  console.log(receiver.name, receiverStart, receiverEnd);
+  let start = (receiver.center_frequency - receiver.bandwidth / 2) / 1000000;
+  let end = (receiver.center_frequency + receiver.bandwidth / 2) / 1000000;
 
-  // Check each band filter to ensure the receiver fully covers the band.
-  if (filters['160m'] && (receiverStart > 1800 || receiverEnd < 2000)) {
-    return false;
-  }
-  if (filters['80m'] && (receiverStart > 3500 || receiverEnd < 4000)) {
-    return false;
-  }
-  if (filters['40m'] && (receiverStart > 7000 || receiverEnd < 7300)) {
-    return false;
-  }
-  if (filters['20m'] && (receiverStart > 14000 || receiverEnd < 14350)) {
-    return false;
-  }
-  if (filters['2m'] && (receiverStart > 144000 || receiverEnd < 148000)) { // Adjusted values to typical 2m band
-    return false;
-  }
-
-  // If none of the conditions triggered a false return, the receiver is considered a match.
-  return true;
+  return filterFrequency >= start && filterFrequency <= end;
 }
 
-
-
-function getTotalUserCount(receivers) {
-  var count = 0;
-  for (var i = 0; i < receivers.length; i++) {
-    count += receivers[i].users;
-  }
-  return count;
-}
-
-function Stats(props) {
-
-  return (
-
-    <div className='text-white mt-5 rounded bg-slate-800 w-72'>
-
-      <p className='font-bold p-2 bg-gradient-to-r from-sky-500 to-indigo-400 text-center h-6 flex items-center justify-center rounded-t'><i className="fa-solid fa-circle-info w-6 -h"></i> Stats</p>
-
-      <div className='flex flex-col p-2 border-l border-b border-r rounded-b border-sky-500'>
-        <table className='text-white w-full'>
-
-          <tbody>
-
-            <tr>
-              <td className='w-64'>
-                Total Receivers:
-              </td>
-
-              <td className='font-mono'>
-                {props.receivers.length}
-              </td>
-            </tr>
-
-            <tr>
-
-              <td className='w-48'>
-                Total Users:
-              </td>
-
-              <td className='font-mono'>
-                {getTotalUserCount(props.receivers)}
-              </td>
-            </tr>
-
-          </tbody>
-
-        </table>
-      </div>
-
-    </div>
-
-
-  )
-}
-
-function Filters(props) {
-
-
-  const toggleFilter = (band) => {
-    props.setFilters((prevFilters) => ({
-      ...prevFilters,
-      [band]: !prevFilters[band],
-    }));
-  };
-
-  return (
-
-    <div className='text-white mt-5 rounded bg-slate-800 w-72'>
-
-      <p className='font-bold p-2 bg-gradient-to-r from-sky-500 to-indigo-400 text-center h-6 flex items-center justify-center rounded-t'><i className="fa-solid fa-filter w-6"></i> Filters</p>
-
-      <div className='flex flex-col items-center justify-between rounded-b p-2 border-l border-b border-r border-sky-400'>
-        {Object.keys(props.filters).map((band) => (
-          <button
-            key={band}
-            className={`block w-full my-1 rounded border border-slate-600 ${props.filters[band] ? 'bg-green-400  text-black font-bold' : 'bg-gradient-to-r from-slate-700 to-slate-800'}`}
-            onClick={() => toggleFilter(band)}
-          >
-            {band}
-          </button>
-        ))}
-      </div>
-
-    </div>
-
-  )
-}
 
 function getSortedAndFilteredReceiverList(props) {
 
   const receiverlist = props.receivers
-    .filter(receiver => checkReceiverFilter(props.filters, receiver))
-    .sort((a, b) => b.users - a.users); // Sorting in descending order by user count
+    .filter(receiver => checkReceiverFilter(props.filterFrequency, receiver))
+    .sort((a, b) => b.users - a.users);
 
   return receiverlist;
 }
 
 
-function SDRList(props) {
+function gridLocatorToLatLng(gridLocator) {
 
+  const re = /^[A-R]{2}[0-9]{2}([A-X]{2})?([0-9]{2})?$/i;
+  if (!re.test(gridLocator)) {
+    throw new Error('Invalid grid locator format');
+  }
+
+  gridLocator = gridLocator.toUpperCase();
+
+  let lng = -180.0;
+  let lat = -90.0;
+
+  lng += (gridLocator.charCodeAt(0) - 'A'.charCodeAt(0)) * 20;
+  lat += (gridLocator.charCodeAt(1) - 'A'.charCodeAt(0)) * 10;
+
+  if (gridLocator.length >= 4) {
+    lng += parseInt(gridLocator[2]) * 2;
+    lat += parseInt(gridLocator[3]) * 1;
+
+    if (gridLocator.length >= 6) {
+      lng += (gridLocator.charCodeAt(4) - 'A'.charCodeAt(0)) * 5 / 60;
+      lat += (gridLocator.charCodeAt(5) - 'A'.charCodeAt(0)) * 2.5 / 60;
+
+      if (gridLocator.length === 8) {
+        lng += parseInt(gridLocator[6]) * 5 / 600;
+        lat += parseInt(gridLocator[7]) * 2.5 / 600;
+      }
+    }
+  }
+
+  if (gridLocator.length === 2) {
+    lng += 10;
+    lat += 5;
+  } else if (gridLocator.length === 4) {
+    lng += 1;
+    lat += 0.5;
+  } else if (gridLocator.length === 6) {
+    lng += 2.5 / 60;
+    lat += 1.25 / 60;
+  } else if (gridLocator.length === 8) {
+    lng += 2.5 / 600;
+    lat += 1.25 / 600;
+  }
+
+  return [lat, lng];
+}
+
+function updateReceiverList(setReceivers) {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const receiversData = await fetchReceivers();
+      const updates = receiversData.map(async receiver => {
+        receiver.country = await getCountry(receiver) || receiver.country;
+        return receiver;
+      });
+
+      Promise.all(updates).then(updatedReceivers => {
+        setReceivers(updatedReceivers);
+      });
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 10 * 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+}
+
+
+function SDRList(props) {
   return (
 
-    <div className='text-white flex flex-col justify-center items-center w-72 my-2'>
+    <div className='text-black dark:text-white items-center w-72 my-2'>
 
-      {getSortedAndFilteredReceiverList(props).map((receiver) =>
+      {getSortedAndFilteredReceiverList(props).map((receiver, index) =>
 
-        <a key={receiver.id} className='relative shadow-inner shadow-slate-700 border border-slate-600 p-2 rounded bg-slate-800 m-1 w-full mt-5' target="_blank" href={getLink(receiver)}>
+        <a
+          key={receiver.id}
+          className='w-full block opacity-0 shadow shadow-gray-400 dark:shadow-slate-700 border border-gray-300 dark:border-slate-600 p-2 rounded bg-white dark:bg-slate-800 m-1 w-full my-2 animate-scaleIn'
+          style={{ animationDelay: `${200 * index}ms` }} // Inline style for delay
+          target="_blank"
+          href={getLink(receiver)}
+        >
 
-          <div className='flex items-center justify-center'>
-            <p className='font-bold truncate'>{receiver.name}</p>
-          </div>
+          <p className='font-bold truncate text-center'>{receiver.name}</p>
 
-          <div id="stats" className='flex justify-center items-center flex-col mt-2'>
+          <div className='flex justify-center items-center flex-col mt-2'>
 
             <table className='table-fixed'>
 
               <tbody>
 
-                <tr className='border-b border-slate-700'>
+                <tr className='border-b border-gray-300 dark:border-slate-700'>
                   <td className='flex'>
 
                     <div className='flex justify-center items-center w-6'>
@@ -190,11 +172,11 @@ function SDRList(props) {
                     <span className='pl-2'>Location</span>
                   </td>
                   <td>
-                    <img className='w-6 block bg-slate-100' src={getFlagImage(receiver)} />
+                    <img className='w-6 block bg-gray-200 dark:bg-slate-100' src={getFlagImage(receiver)} />
                   </td>
                 </tr>
 
-                <tr className='border-b border-slate-700'>
+                <tr className='border-b border-gray-300 dark:border-slate-700'>
                   <td className='flex'>
 
                     <div className='flex justify-center items-center w-6'>
@@ -208,7 +190,7 @@ function SDRList(props) {
                   </td>
                 </tr>
 
-                <tr className='border-b border-slate-700'>
+                <tr className='border-b border-gray-300 dark:border-slate-700'>
                   <td className='flex'>
 
                     <div className='flex justify-center items-center w-6'>
@@ -236,8 +218,6 @@ function SDRList(props) {
                   </td>
                 </tr>
 
-
-
               </tbody>
 
             </table>
@@ -248,71 +228,16 @@ function SDRList(props) {
 
       )}
     </div>
-
-  )
+  );
 }
 
-function gridLocatorToLatLng(gridLocator) {
-  // Ensure the grid locator is in the correct format (2, 4, 6 or 8 characters)
-  const re = /^[A-R]{2}[0-9]{2}([A-X]{2})?([0-9]{2})?$/i;
-  if (!re.test(gridLocator)) {
-    throw new Error('Invalid grid locator format');
-  }
 
-  gridLocator = gridLocator.toUpperCase();
-
-  // Latitude and longitude start values
-  let lng = -180.0;
-  let lat = -90.0;
-
-  // Add longitude for first pair (fields)
-  lng += (gridLocator.charCodeAt(0) - 'A'.charCodeAt(0)) * 20;
-  lat += (gridLocator.charCodeAt(1) - 'A'.charCodeAt(0)) * 10;
-
-  // If the locator is not just a field
-  if (gridLocator.length >= 4) {
-    // Add longitude for second pair (squares)
-    lng += parseInt(gridLocator[2]) * 2;
-    lat += parseInt(gridLocator[3]) * 1;
-
-    // If the locator includes subsquares
-    if (gridLocator.length >= 6) {
-      // Add longitude for third pair (subsquares)
-      lng += (gridLocator.charCodeAt(4) - 'A'.charCodeAt(0)) * 5 / 60;
-      lat += (gridLocator.charCodeAt(5) - 'A'.charCodeAt(0)) * 2.5 / 60;
-
-      // If the locator includes extended subsquares
-      if (gridLocator.length === 8) {
-        // Add longitude for fourth pair (extended subsquares)
-        lng += parseInt(gridLocator[6]) * 5 / 600;
-        lat += parseInt(gridLocator[7]) * 2.5 / 600;
-      }
-    }
-  }
-
-  // The lat/lng is at the southwest corner of the square; adjust to center
-  if (gridLocator.length === 2) {
-    lng += 10;
-    lat += 5;
-  } else if (gridLocator.length === 4) {
-    lng += 1;
-    lat += 0.5;
-  } else if (gridLocator.length === 6) {
-    lng += 2.5 / 60;
-    lat += 1.25 / 60;
-  } else if (gridLocator.length === 8) {
-    lng += 2.5 / 600;
-    lat += 1.25 / 600;
-  }
-
-  return [lat, lng];
-}
 
 
 function SDRMap(props) {
 
   return (
-    <div className='rounded-2xl bg-slate-500 mt-5 w-full h-96' >
+    <div className='flex-grow rounded-2xl mt-5 w-full h-full p-5' >
       <MapContainer className='rounded-2xl w-full h-full' center={[51.00, 9.00]} zoom={2} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -354,118 +279,130 @@ function SDRMap(props) {
 
 function ModeSwitchButton(props) {
   return (
-    <div className='flex text-white mt-5'>
-
+    <div className='flex text-black dark:text-white my-2'>
       <div className='flex justify-center items-center'>
         <i className="fa-solid fa-list"></i>
         <p className='ml-2'>List</p>
       </div>
 
-      <div className="relative flex items-center w-12 rounded-full bg-slate-500 border border-slate-400 mx-2 cursor-pointer " onClick={() => props.setMapView(!props.mapView)}>
-        <div className={"absolute w-4 h-4 rounded-full bg-slate-200 border border-slate-700 m-1 " + (props.mapView ? 'right-0' : 'left-0')}></div>
+      <div className="relative flex items-center w-12 rounded-full bg-gray-300 dark:bg-slate-500 border border-gray-400 dark:border-slate-400 mx-2 cursor-pointer" onClick={() => props.setMapView(!props.mapView)}>
+        <div className={`absolute w-4 h-4 rounded-full bg-white dark:bg-slate-200 border border-gray-500 dark:border-slate-700 m-1 ${props.mapView ? 'right-0' : 'left-0'}`}></div>
       </div>
 
       <div className='flex justify-center items-center'>
         <i className="fa-solid fa-map"></i>
         <p className='ml-2'>Map</p>
       </div>
-
     </div>
-  )
+  );
 }
+
 
 
 function CurrentlyNoReceivers() {
   return (
     <div className='mt-5 text-white flex flex-col w-full items-center justify-center'>
       <i className="text-5xl inline fa-regular fa-circle-xmark"></i>
-
       <p className='text-2xl mt-2'>Currently No Receivers</p>
     </div>
   )
 }
 
-function getFlagImage(receiver) {
-  return `https://flagcdn.com/${receiver.country}.svg`;
+
+
+function FrequencyFilter(props) {
+
+  const handleFrequencyInputChange = (event) => {
+    const value = event.target.value;
+    const regex = /^[0-9]*\.?[0-9]*$/;
+    if (value === '') {
+      props.setFilterFrequency(-1);
+    } else if (regex.test(value)) {
+      props.setFilterFrequency(Number(value));
+    }
+  };
+  return (
+    <div className='flex flex-row items-center justify-between rounded-b my-2'>
+
+      <input
+        type="text"
+        className='dark:text-white dark:bg-slate-700 dark:border-slate-600 bg-gray-100 rounded p-1 border border-gray-200'
+        placeholder='Filter Frequency (Mhz)'
+        onChange={handleFrequencyInputChange}
+        pattern="[0-9]*"
+      />
+    </div>
+  )
 }
 
-async function getCountry(receiver) {
-  try {
-    let response = await fetch(`https://api.country.is/${receiver.ip}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    let json = await response.json();
-    return json.country.toLowerCase();
-  } catch (error) {
-    console.error("Could not fetch country: ", error);
+
+function DarkModeToggleButton(props) {
+
+  let icon = "fa-sun";
+
+  if (props.darkMode) {
+    icon = "fa-sun"
+  } else {
+    icon = "fa-moon"
   }
+
+  const toggleDarkMode = () => {
+    props.setDarkMode(!props.darkMode);
+
+    if (props.darkMode) {
+      document.body.classList.remove("dark");
+
+    } else {
+      document.body.classList.add("dark");
+    }
+  }
+
+  return (
+    <button onClick={toggleDarkMode} className='absolute w-8 h-8 top-1 right-2 flex items-center justify-center dark:text-white p-2 '>
+      <i className={"fa-regular " + icon}></i>
+    </button>
+  )
 }
 
 function App() {
 
   const [receivers, setReceivers] = useState([]);
   const [mapView, setMapView] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
+  const [filterFrequency, setFilterFrequency] = useState(-1);
 
-  const [filters, setFilters] = useState({
-    '160m': false,
-    '80m': false,
-    '40m': false,
-    '20m': false,
-    '2m': false
-  });
-
-  useEffect(() => {
-
-    const fetchData = async () => {
-      const receiversData = await fetchReceivers();
-      const updates = receiversData.map(async receiver => {
-        receiver.country = await getCountry(receiver) || receiver.country;
-        return receiver;
-      });
-
-      Promise.all(updates).then(updatedReceivers => {
-        setReceivers(updatedReceivers);
-      });
-    };
-
-    fetchData();
-    const intervalId = setInterval(fetchData, 1000 * 10);
-
-    return () => clearInterval(intervalId);
-  }, []);
+  updateReceiverList(setReceivers);
 
   return (
     <>
-      <div className='bg-slate-800 w-full border-b border-slate-600 p-2'>
-        <h1 className='text-white text-center font-bold'>SDR List</h1>
-      </div>
+      <div className="flex flex-col items-center select-none h-full dark:bg-slate-900 ">
+        <div className='dark:bg-slate-800 w-full border-b dark:border-slate-600 border-slate-300 p-2'>
+          <h1 className='dark:text-white text-slate-500 text-center font-bold'>SDR List</h1>
+        </div>
+        <a href="https://github.com/Steven9101/PhantomSDR-Plus" className='font-bold text-center my-2 text-sky-500 underline'><i className="fa-solid fa-link"></i> Guide: Host Your Own SDR</a>
 
-      <div className='flex flex-col items-center justify-center select-none m-5'>
 
-        <a href="https://github.com/Steven9101/PhantomSDR-Plus" className='font-bold text-center mt-3 text-sky-500 underline'><i className="fa-solid fa-link"></i> Guide: Host Your Own SDR</a>
+        <DarkModeToggleButton darkMode={darkMode} setDarkMode={setDarkMode}></DarkModeToggleButton>
+
+        <FrequencyFilter filterFrequency={filterFrequency} setFilterFrequency={setFilterFrequency}></FrequencyFilter>
 
         {receivers.length === 0 ? (
           <CurrentlyNoReceivers></CurrentlyNoReceivers>
         ) : (
           <>
-
-            <Stats receivers={receivers}></Stats>
-            <Filters filters={filters} setFilters={setFilters}></Filters>
             <ModeSwitchButton mapView={mapView} setMapView={setMapView}></ModeSwitchButton>
-
             {mapView ? (
-              <SDRMap receivers={receivers} filters={filters}></SDRMap>
+              <SDRMap receivers={receivers} filterFrequency={filterFrequency}></SDRMap>
             ) : (
-              <SDRList receivers={receivers} filters={filters}></SDRList>
+              <SDRList receivers={receivers} filterFrequency={filterFrequency}></SDRList>
             )}
-
           </>
         )}
       </div>
     </>
   );
 }
+
 
 
 export default App
